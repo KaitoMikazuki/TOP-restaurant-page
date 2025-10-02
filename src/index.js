74d8a29c89ca3bda9b2e0ba5bef6157e08b24cf7
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-10-02 11:06:43 +0800

    Import loadPage function and add DOMContentLoaded event listener

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 console.log("Hello wrld")
 
 import "./styles.css";
+import {loadPage} from "./homepage.js"
+
+let contentId = document.getElementById("content");
+contentId.addEventListener("DOMContentLoaded", loadPage())
+
+// WHEN PAGE LOADED TRIGGER HOMEPAGE.JS
 // I might need to import every svg
