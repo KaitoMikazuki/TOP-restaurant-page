4948e8ecfaa6d3d5d957e3c2cbd974e8e767ed98
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-10-06 14:58:25 +0800

    Add clearPage function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-console.log("Hello wrld")
-
 import "./styles.css";
 import {loadPage} from "./homepage.js"
+console.log("Hello world")
 
 let content = document.getElementById("content");
-    content.addEventListener("DOMContentLoaded", loadPage(content))
+content.addEventListener("DOMContentLoaded", loadPage(content));
 
 // WHEN PAGE LOADED TRIGGER HOMEPAGE.JS
 // I might need to import every svg
+
+function clearPage (content){
+    content.innerHTML = "";
+}
