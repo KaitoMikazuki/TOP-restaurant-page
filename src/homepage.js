cc839f87670e2f12d5e50ff813f79e704b9a6774
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-10-06 13:29:08 +0800

    Configure webpack to load videos and images

--- src/homepage.js
+++ src/homepage.js
@@ -1,3 +1,5 @@
+const video_attributes = ["autoplay", "muted", "loop", "playsinline", "preload"]
+
 export function loadPage() {
     console.log("homepage.js loaded")
     if (!Document){
@@ -20,17 +22,23 @@ export function loadPage() {
     const text = document.createElement("p");
     text.textContent = "For only ₱139, you can indulge in our very own JAN TAYO, POTATOL’S PABORITO, and KYNAN’S BEST—layered to perfection and coated with iconic international flavors! See you there! 🍗🍟🪽";
 
-
     const coa = document.createElement("div");
     coa.classList.add("coa");
-        let button = document.createElement("button");
+        const button = document.createElement("button");
         button.classList.add("button")
         button.textContent = "Pre-order";
-        let menu = document.createElement("button");
+        const menu = document.createElement("button");
         menu.textContent = "View Menu";
         coa.append(button, menu);
 
     hero.append(header, text, coa);
-    
-        
+
+    const video = document.createElement("video");
+    addAttributes(video_attributes, video);
+
+    homepage.appendChild(video);        
+}
+
+function addAttributes(attributesList, video){
+    attributesList.forEach((attribute) => video.setAttribute(attribute, ""));
 }
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,14 @@ module.exports = {
             test: /\.html$/i,
             loader: "html-loader",
         },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
+        {
+            test: /\.(mov|mp4|webm|ogg|ogv|avi|wmv)$/, // Match common video file extensions
+            type: 'asset/resource', // Use asset/resource type
+        },
     ],
   },
 };
