af9930bd953b6ebea817c5e730216823b45c9ee7
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-10-07 10:59:09 +0800

    Fix syntax for intended use

--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,22 @@
-function FoodItem(name, price, id, description){
-    this.name = name;
-    this.price = price
-    this.id = id
-    this.description = description
-    // image link?
+const FoodItem = function(name, price, id, description){
+    let item = {}
+    item.name = name;
+    item.price = price;
+    item.id = id;
+    item.description = description;
+    return item
+
 }
 
 const menu_items = []
-menu_items.append(FoodItem("Jan Tayo!", 139, 1, "Garlic Parmesan + Curry Ketchup"));
-menu_items.append(FoodItem("Potatol's Paborito", 139, 2, "Soy Garlic + Garlic Mayonaise"));
-menu_items.append(FoodItem("Kynan's Best", 139, 3, "Buffalo + Cheese"));
+menu_items.push(FoodItem("Jan Tayo!", 139, 1, "Garlic Parmesan + Curry Ketchup"));
+menu_items.push(FoodItem("Potatol's Paborito", 139, 2, "Soy Garlic + Garlic Mayonaise"));
+menu_items.push(FoodItem("Kynan's Best", 139, 3, "Buffalo + Cheese"));
 //I lowkey want to use sheets with this
 
 
 export function loadMenu(content){
-
-
+    console.log(menu_items);
 }
 
 function loadFood(){
